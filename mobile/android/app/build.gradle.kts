bdb40b1da10bade0e05abb2a63dfa41c5aee5230
DQM27 <daniel.bleach1@gmail.com>
2026-09-08 13:40:03 -0600

    Preparar release movil 1.1.2

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         // y x86_64 (emulador de desarrollo).
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.1"
+        versionCode = 3
+        versionName = "1.1.2"
     }
 
     signingConfigs {
